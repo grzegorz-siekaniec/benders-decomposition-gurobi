d15df4c2c3d8908e2576bc599c4f9ec5ec2576af
grzegorz.siekaniec <grzegorz.siekaniec@gmail.com>
2021-06-01 21:44:27 +0200

    Improving code further: using interpolation during logging etc.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # benders-decomposition-gurobi
 
 Repository contains implementation of Bender Decomposition for classical facility/warehause location problem using Python and Gurobi solver.
+
+Implementation is not intendent to be fast but rather descriptive.
+
+See https://grzegorz-siekaniec.github.io/bits-of-this-bits-of-that/2021/may.html for more details.
+
+TODO: Improve description
--- benders-decomposition/src/benders_decomposition/solver.py
+++ benders-decomposition/src/benders_decomposition/solver.py
@@ -19,42 +19,48 @@ def solve_using_benders_decomposition(input_data: InputData):
     master_problem = MasterProblemBuilder(input_data).build()
     sub_problem = SubProblemBuilder(input_data).build()
 
-    rhs = create_sub_problem_constraint_to_master_column_map(master_problem, sub_problem, input_data)
+    mapping = create_sub_problem_constraint_to_master_column_or_value_map(master_problem, sub_problem, input_data)
 
-    master_problem.register_callback(cb_benders(master_problem, sub_problem, rhs, input_data))
+    master_problem.register_callback(cb_benders(master_problem, sub_problem, mapping, input_data))
     master_problem.solve()
     master_problem.report_results()
 
-    e = timer()
     logging.info("[END] Solving warehouse location problem using Benders Decomposition."
-                 "It took %f sec.", e - s)
+                 "It took %f sec.", timer() - s)
 
 
-def create_sub_problem_constraint_to_master_column_map(master: MasterProblem,
-                                                       sub_problem: SubProblem,
-                                                       data: InputData)\
+def create_sub_problem_constraint_to_master_column_or_value_map(master: MasterProblem,
+                                                                sub_problem: SubProblem,
+                                                                data: InputData)\
         -> Dict[grb.Constr, Union[grb.Var, float]]:
-    rhs = dict()
+    """
+    Creates a mapping between sub-problem constraint and elements of (b - By).
+    :param master: instance of MasterProblem
+    :param sub_problem: instance of Subproblem
+    :param data: input data
+    :return: dictionary with mapping
+    """
+    mapping = dict()
     for facility_name, row in sub_problem.facility_to_supply_constraint.items():
-        rhs[row] = -data.supply(facility_name) * master.name_to_column[facility_name]
+        mapping[row] = -data.supply(facility_name) * master.name_to_column[facility_name]
 
     for customer_name, row in sub_problem.customer_to_demand_constraint.items():
-        rhs[row] = row.getAttr(grb.GRB.Attr.RHS)
+        mapping[row] = row.getAttr(grb.GRB.Attr.RHS)
 
-    return rhs
+    return mapping
 
 
 def cb_benders(master: MasterProblem,
-               subproblem: SubProblem,
-               subproblem_constraints_to_master_columns: Dict[grb.Constr, Union[grb.Var, float]],
+               sub_problem: SubProblem,
+               mapping: Dict[grb.Constr, Union[grb.Var, float]],
                data: InputData):
     """
     Benders Decomposition callback
-    :param master:
-    :param subproblem:
-    :param subproblem_constraints_to_master_columns: holds (b - By) mapping
+    :param master: instance of MasterProblem
+    :param sub_problem: instance of SubProblem
+    :param mapping: mapping between sub-problem constraint and and elements of (b - By) vector.
     :param data: input data
-    :return:
+    :return: inner callback function.
     """
     def callback_inner(model, where):
 
@@ -68,45 +74,45 @@ def cb_benders(master: MasterProblem,
             sub_problem_rhs = {facility_name: -data.supply(facility_name) if utils.is_non_zero(val) else 0.0
                                for facility_name, val in zip(facility_names, mp_facility_values)}
 
-            subproblem.set_supply_constraint_rhs(sub_problem_rhs)
+            sub_problem.set_supply_constraint_rhs(sub_problem_rhs)
 
             # Solve sub-problem
-            subproblem.solve()
+            sub_problem.solve()
 
-            logging.debug(f'Subproblem status: {subproblem.model.getAttr(grb.GRB.Attr.Status)}')
+            logging.debug(f'Subproblem status: {sub_problem.model.getAttr(grb.GRB.Attr.Status)}')
 
             # Add cuts (lazy constraints) based on sub-problem status
-            if subproblem.status() == grb.GRB.Status.INFEASIBLE:
+            if sub_problem.status() == grb.GRB.Status.INFEASIBLE:
 
                 # Add feasibility cut
                 cut = []
-                for facility_name, row in subproblem.facility_to_supply_constraint.items():
+                for facility_name, row in sub_problem.facility_to_supply_constraint.items():
                     dual_farkas = row.getAttr(grb.GRB.Attr.FarkasDual)
                     logging.debug(f'{facility_name}: {dual_farkas}')
-                    cut.append(dual_farkas * subproblem_constraints_to_master_columns[row])
+                    cut.append(dual_farkas * mapping[row])
 
-                for customer_name, row in subproblem.customer_to_demand_constraint.items():
+                for customer_name, row in sub_problem.customer_to_demand_constraint.items():
                     dual_farkas = row.getAttr(grb.GRB.Attr.FarkasDual)
                     logging.debug(f'{customer_name}: {dual_farkas}')
-                    cut.append(dual_farkas * subproblem_constraints_to_master_columns[row])
+                    cut.append(dual_farkas * mapping[row])
 
                 logging.debug("Adding feasibility cut: ", grb.quicksum(cut) >= 0)
                 model.cbLazy(grb.quicksum(cut) >= 0)
 
-            elif subproblem.status() == grb.GRB.Status.OPTIMAL:
-                sub_problem_obj_val = subproblem.model.getAttr(grb.GRB.Attr.ObjVal)
+            elif sub_problem.status() == grb.GRB.Status.OPTIMAL:
+                sub_problem_obj_val = sub_problem.model.getAttr(grb.GRB.Attr.ObjVal)
                 z = master.name_to_column[master.aux_var_name]
                 z_val = model.cbGetSolution(z)
                 if utils.is_non_zero(sub_problem_obj_val - z_val):
 
                     # Add optimality cut
                     cut = []
-                    for _, row in subproblem.facility_to_supply_constraint.items():
+                    for _, row in sub_problem.facility_to_supply_constraint.items():
                         dual_supply = row.getAttr(grb.GRB.Attr.Pi)
-                        cut.append(dual_supply * subproblem_constraints_to_master_columns[row])
-                    for _, row in subproblem.customer_to_demand_constraint.items():
+                        cut.append(dual_supply * mapping[row])
+                    for _, row in sub_problem.customer_to_demand_constraint.items():
                         dual_demand = row.getAttr(grb.GRB.Attr.Pi)
-                        cut.append(dual_demand * subproblem_constraints_to_master_columns[row])
+                        cut.append(dual_demand * mapping[row])
 
                     cut.append(-z)
                     model.cbLazy(grb.quicksum(cut) <= 0)
